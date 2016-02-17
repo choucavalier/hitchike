0e0615828899bd057f5c2bca95a9b3fa6d3f6883
toogy <toogy@too.gy>
2016-02-17 18:20:08 +0100

    Split settings solved thanks to django-split-settings

---
+++ hitchike/settings/database.py
@@ -0,0 +1,11 @@
+import os
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
