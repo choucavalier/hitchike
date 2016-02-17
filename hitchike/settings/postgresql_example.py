8be75266f2c0894073b1dd1c3478d9df045be3df
toogy <toogy@too.gy>
2016-02-17 18:22:07 +0100

    Adding postgresql config example for deployment

---
+++ hitchike/settings/postgresql_example.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hitchike',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
